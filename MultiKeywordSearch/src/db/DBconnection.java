72bbdadf9740eacd45ad36695795033f6800208c
Bryan Wodi <talk2kamp@gmail.com>
2019-04-09 03:21:05 -0500

    remoived unnecessary imports

--- MultiKeywordSearch/src/db/DBconnection.java
+++ MultiKeywordSearch/src/db/DBconnection.java
@@ -11,12 +11,9 @@ package db;
  */
 
 
-import java.io.FileInputStream;
-import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBconnection {
 
